3d51b9aba54642f9fa809fd4ef13a4b53841a4d7
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2019-07-07 22:59:29 +0200

    offer create

---
+++ main/choices.py
@@ -0,0 +1,57 @@
+CATEGORY = (
+    (1, ("Osobowy")),
+    (2, ("Dostawczy")),
+    (3, ("Ciężarowy")),
+    (4, ("Motocykl")),
+    (5, ("Inne")),
+)
+
+STAN = (
+    (1, ("Używany")),
+    (2, ("Nowy")),
+)
+
+STATUS = (
+    (1, ("Nieuszkodzony, bezwypadkowy")),
+    (2, ("Uszkodzony, bezwypadkowy")),
+    (3, ("Nieuszkodzony, powypadkowy")),
+    (4, ("Uszkodzony, powypadkowy")),
+)
+
+TYPE = (
+    (1, ("Sedan")),
+    (2, ("Kombi")),
+    (3, ("Coupe")),
+    (4, ("Limuzyna")),
+    (5, ("Hatchback")),
+    (6, ("Kabriolet")),
+    (7, ("Pick-up")),
+    (8, ("Van")),
+    (9, ("SUV")),
+    (10,("Roadster")),
+    (11,("Inne")),
+)
+
+FUEL = (
+    (1, ("Benzyna")),
+    (2, ("Diesel")),
+    (3, ("Elektryczny")),
+    (4, ("Hybryda")),
+    (5, ("LPG")),
+)
+
+GEAR_BOX = (
+    (1, ("Manualna")),
+    (2, ("Automatyczna")),
+)
+
+NAPED = (
+    (1, ("Przednia oś")),
+    (2, ("Tylna oś")),
+    (2, ("Na cztery ko")),
+)
+
+
+
+
+
---
+++ samochody.py
@@ -0,0 +1,5 @@
+from main.models import CarMark, CarModel
+
+with open(file='asd.txt') as p:
+    for line in p:
+        print(line)
